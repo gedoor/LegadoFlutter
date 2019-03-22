a4ceb07685cf63da963b71a6f6e89bf9e2ff5cfc
Antecer <ymh441915964@gmail.com>
2019-03-22 21:51:34 +0800

    封装bookCard部件

--- lib/pages/index.dart
+++ lib/pages/index.dart
@@ -32,22 +32,38 @@ class IndexPageState extends State<IndexPage> {
       // ),
       // 主体body部分
       body: Container(
-          color: Color(0xFFFFFFFF),
-          child: ListView(
-            children: <Widget>[
-              BookCard(),
-              BookCard(),
-              BookCard(),
-              BookCard(),
-              BookCard(),
-              BookCard(),
-              BookCard(),
-              BookCard(),
-              BookCard(),
-              BookCard(),
-              BookCard(),
-            ],
-          ),
+        color: Color(0xFFFFFFFF),
+        child: ListView(
+          children: <Widget>[
+            BookCard(
+              bookName: "牧神记",
+              bookAuthor: "宅猪",
+              bookCover: "https://www.biqubao.com/cover/18/18569/18569s.jpg",
+              bookMark: "第一章 天黑别出门",
+              bookLastChapter: "第一五零五章 道问",
+              bookUnreadCount: 1572-1,
+              hasUpdate: true,
+            ),
+            BookCard(
+              bookName: "修真聊天群",
+              bookAuthor: "圣骑士的传说",
+              bookCover: "https://www.biqubao.com/cover/2/2324/2324s.jpg",
+              bookMark: "第二十四章 宋书航同学，有你的大快递",
+              bookLastChapter: "第2575章 要不要盘根霸神柱之金龙柱出",
+              bookUnreadCount: 2615-31,
+              hasUpdate: true,
+            ),
+            BookCard(
+              bookName: "科技霸权",
+              bookAuthor: "秒速九光年",
+              bookCover: "https://www.biqubao.com/cover/25/25075/25075s.jpg",
+              bookMark: "第三百一十八章 下一代计算规则，将由我们来制定！",
+              bookLastChapter: "第三百二十章 事情闹大了，博登湖惨案",
+              bookUnreadCount: 2,
+              hasUpdate: false,
+            ),
+          ],
+        ),
       ),
     );
   }
--- lib/widgets/bookListCard.dart
+++ lib/widgets/bookListCard.dart
@@ -1,10 +1,38 @@
-import 'dart:convert';
-import 'dart:typed_data';
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
 
 /// 书籍卡片
 class BookCard extends StatefulWidget {
+  /// 书籍名称
+  final String bookName;
+
+  /// 书籍作者
+  final String bookAuthor;
+
+  /// 封面地址
+  final String bookCover;
+
+  /// 书签章节
+  final String bookMark;
+
+  /// 最新章节
+  final String bookLastChapter;
+
+  /// 未读章节数
+  final int bookUnreadCount;
+
+  /// 是否有新章节
+  final bool hasUpdate;
+
+  BookCard(
+      {@required this.bookName,
+      this.bookAuthor,
+      this.bookCover,
+      this.bookMark,
+      this.bookLastChapter,
+      this.bookUnreadCount,
+      this.hasUpdate});
+
   @override
   State createState() => new BookCardState();
 }
@@ -25,7 +53,7 @@ class BookCardState extends State<BookCard> {
               ClipRRect(
                 borderRadius: BorderRadius.circular(4.0),
                 child: Image.network(
-                  "https://www.biqubao.com/cover/0/144/144s.jpg",
+                  "${widget.bookCover}",
                   width: 60.0,
                   height: 80.0,
                   fit: BoxFit.fill,
@@ -33,53 +61,63 @@ class BookCardState extends State<BookCard> {
               ),
               Expanded(
                 child: Container(
-                  margin: EdgeInsets.only(left: 8.0),
+                  margin: EdgeInsets.only(left: 6.0),
                   child: Column(
                     crossAxisAlignment: CrossAxisAlignment.start,
                     children: <Widget>[
-                      Row(
-                        children: <Widget>[
-                          // 书籍名称
-                          Expanded(
-                            child: Text(
-                              "凡人修仙传",
-                              style: TextStyle(
-                                fontWeight: FontWeight.bold,
-                                fontSize: 20.0,
-                              ),
-                              maxLines: 1,
-                            ),
-                          ),
-                          // 右上角未读章节数
-                          ClipRRect(
-                            borderRadius: BorderRadius.circular(8.0),
-                            child: Container(
-                              padding: EdgeInsets.symmetric(horizontal: 4.0),
-                              color: Color(0xFF888888), // 背景颜色
+                      Container(
+                        margin: EdgeInsets.only(bottom: 4.0),
+                        child: Row(
+                          children: <Widget>[
+                            // 书籍名称
+                            Expanded(
                               child: Text(
-                                "1234",
-                                style:
-                                    TextStyle(color: Color(0xFFFFFFFF) // 字体颜色
-                                        ),
+                                "${widget.bookName}",
+                                style: TextStyle(
+                                  fontFamily: "KTGB2312", // 字体名称
+                                  fontWeight: FontWeight.normal, // 字重
+                                  fontSize: 16.0, // 字号
+                                ),
+                                maxLines: 1,
                               ),
                             ),
-                          ),
-                        ],
+                            // 右上角未读章节数
+                            ClipRRect(
+                              borderRadius: BorderRadius.circular(8.0),
+                              child: Container(
+                                padding: EdgeInsets.symmetric(horizontal: 4.0),
+                                color: widget.hasUpdate
+                                    ? Color(0xFFFF0000)
+                                    : Color(0xFF888888), // 背景颜色
+                                child: Text(
+                                  "${widget.bookUnreadCount}",
+                                  style: TextStyle(
+                                    color: Color(0xFFFFFFFF), // 字体颜色
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
                       ),
                       // 书籍作者
                       Row(
                         children: <Widget>[
                           Icon(
-                            Icons.account_box,  // 作者图标
+                            Icons.account_box, // 作者图标
                             size: 18.0,
                           ),
                           Text(
-                            "忘语",
+                            "${widget.bookAuthor}",
                             maxLines: 1,
+                            style: TextStyle(
+                              fontFamily: "KTGB2312",
+                              fontSize: 12.0, // 字号
+                            ),
                           ),
                         ],
                       ),
-                      // 阅读进度
+                      // 书签章节
                       Row(
                         children: <Widget>[
                           Icon(
@@ -88,9 +126,13 @@ class BookCardState extends State<BookCard> {
                           ),
                           Expanded(
                             child: Text(
-                              "第三十八章 柳暗花明",
+                              "${widget.bookMark}",
                               maxLines: 1,
-                              overflow: TextOverflow.ellipsis,  // 溢出的文本用省略号代替
+                              overflow: TextOverflow.ellipsis, // 溢出的文本用省略号代替
+                              style: TextStyle(
+                                fontFamily: "KTGB2312",
+                                fontSize: 11.0, // 字号
+                              ),
                             ),
                           ),
                         ],
@@ -99,14 +141,18 @@ class BookCardState extends State<BookCard> {
                       Row(
                         children: <Widget>[
                           Icon(
-                            Icons.move_to_inbox,  // 最新章节图标
+                            Icons.move_to_inbox, // 最新章节图标
                             size: 18.0,
                           ),
                           Expanded(
                             child: Text(
-                              "第七百八十一章 围追堵截 测试测试测试测试测试",
+                              "${widget.bookLastChapter}",
                               maxLines: 1,
-                              overflow: TextOverflow.ellipsis,  // 溢出的文本用省略号代替
+                              overflow: TextOverflow.ellipsis, // 溢出的文本用省略号代替
+                              style: TextStyle(
+                                fontFamily: "KTGB2312",
+                                fontSize: 11.0, // 字号
+                              ),
                             ),
                           ),
                         ],
