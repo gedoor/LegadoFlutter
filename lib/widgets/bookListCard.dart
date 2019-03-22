73a97eb6def969c5cabfbe2f93e374c1a15f50d7
Antecer <ymh441915964@gmail.com>
2019-03-22 22:10:16 +0800

    调整bookCard结构

--- lib/widgets/bookListCard.dart
+++ lib/widgets/bookListCard.dart
@@ -42,128 +42,128 @@ class BookCardState extends State<BookCard> {
   @override
   Widget build(BuildContext context) {
     return Container(
+      decoration: BoxDecoration(
+        color: Color(0x40666666),
+        borderRadius: BorderRadius.all(Radius.circular(6.0)),
+      ),
       margin: EdgeInsets.symmetric(vertical: 1.0, horizontal: 2.0),
-      child: ClipRRect(
-        borderRadius: BorderRadius.circular(6.0),
-        child: Container(
-          color: Color(0x40666666),
-          padding: EdgeInsets.all(4.0),
-          child: Row(
-            children: <Widget>[
-              ClipRRect(
-                borderRadius: BorderRadius.circular(4.0),
-                child: Image.network(
-                  "${widget.bookCover}",
-                  width: 60.0,
-                  height: 80.0,
-                  fit: BoxFit.fill,
-                ),
-              ),
-              Expanded(
-                child: Container(
-                  margin: EdgeInsets.only(left: 6.0),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: <Widget>[
-                      Container(
-                        margin: EdgeInsets.only(bottom: 4.0),
-                        child: Row(
-                          children: <Widget>[
-                            // 书籍名称
-                            Expanded(
-                              child: Text(
-                                "${widget.bookName}",
-                                style: TextStyle(
-                                  fontFamily: "KTGB2312", // 字体名称
-                                  fontWeight: FontWeight.normal, // 字重
-                                  fontSize: 16.0, // 字号
-                                ),
-                                maxLines: 1,
-                              ),
-                            ),
-                            // 右上角未读章节数
-                            ClipRRect(
-                              borderRadius: BorderRadius.circular(8.0),
-                              child: Container(
-                                padding: EdgeInsets.symmetric(horizontal: 4.0),
-                                color: widget.hasUpdate
-                                    ? Color(0xFFFF0000)
-                                    : Color(0xFF888888), // 背景颜色
-                                child: Text(
-                                  "${widget.bookUnreadCount}",
-                                  style: TextStyle(
-                                    color: Color(0xFFFFFFFF), // 字体颜色
-                                  ),
-                                ),
-                              ),
-                            ),
-                          ],
-                        ),
-                      ),
-                      // 书籍作者
-                      Row(
-                        children: <Widget>[
-                          Icon(
-                            Icons.account_box, // 作者图标
-                            size: 18.0,
-                          ),
-                          Text(
-                            "${widget.bookAuthor}",
-                            maxLines: 1,
+      padding: EdgeInsets.all(4.0),
+      child: Row(
+        children: <Widget>[
+          // 书籍封面
+          ClipRRect(
+            borderRadius: BorderRadius.circular(4.0),
+            child: Image.network(
+              "${widget.bookCover}",
+              width: 60.0,
+              height: 80.0,
+              fit: BoxFit.fill,
+            ),
+          ),
+          // 书籍信息
+          Expanded(
+            child: Container(
+              margin: EdgeInsets.only(left: 6.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Container(
+                    margin: EdgeInsets.only(bottom: 4.0),
+                    child: Row(
+                      children: <Widget>[
+                        // 书籍名称
+                        Expanded(
+                          child: Text(
+                            "${widget.bookName}",
                             style: TextStyle(
-                              fontFamily: "KTGB2312",
-                              fontSize: 12.0, // 字号
+                              fontFamily: "KTGB2312", // 字体名称
+                              fontWeight: FontWeight.normal, // 字重
+                              fontSize: 16.0, // 字号
                             ),
+                            maxLines: 1,
                           ),
-                        ],
-                      ),
-                      // 书签章节
-                      Row(
-                        children: <Widget>[
-                          Icon(
-                            Icons.photo_album, // book 书签图标
-                            size: 18.0,
-                          ),
-                          Expanded(
+                        ),
+                        // 右上角未读章节数
+                        ClipRRect(
+                          borderRadius: BorderRadius.circular(8.0),
+                          child: Container(
+                            padding: EdgeInsets.symmetric(horizontal: 4.0),
+                            color: widget.hasUpdate
+                                ? Color(0xFFFF0000)
+                                : Color(0xFF888888), // 背景颜色
                             child: Text(
-                              "${widget.bookMark}",
-                              maxLines: 1,
-                              overflow: TextOverflow.ellipsis, // 溢出的文本用省略号代替
+                              "${widget.bookUnreadCount}",
                               style: TextStyle(
-                                fontFamily: "KTGB2312",
-                                fontSize: 11.0, // 字号
+                                color: Color(0xFFFFFFFF), // 字体颜色
                               ),
                             ),
                           ),
-                        ],
+                        ),
+                      ],
+                    ),
+                  ),
+                  // 书籍作者
+                  Row(
+                    children: <Widget>[
+                      Icon(
+                        Icons.account_box, // 作者图标
+                        size: 18.0,
                       ),
-                      // 最新章节
-                      Row(
-                        children: <Widget>[
-                          Icon(
-                            Icons.move_to_inbox, // 最新章节图标
-                            size: 18.0,
-                          ),
-                          Expanded(
-                            child: Text(
-                              "${widget.bookLastChapter}",
-                              maxLines: 1,
-                              overflow: TextOverflow.ellipsis, // 溢出的文本用省略号代替
-                              style: TextStyle(
-                                fontFamily: "KTGB2312",
-                                fontSize: 11.0, // 字号
-                              ),
-                            ),
-                          ),
-                        ],
+                      Text(
+                        "${widget.bookAuthor}",
+                        maxLines: 1,
+                        style: TextStyle(
+                          fontFamily: "KTGB2312",
+                          fontSize: 12.0, // 字号
+                        ),
                       ),
                     ],
                   ),
-                ),
+                  // 书签章节
+                  Row(
+                    children: <Widget>[
+                      Icon(
+                        Icons.photo_album, // book 书签图标
+                        size: 18.0,
+                      ),
+                      Expanded(
+                        child: Text(
+                          "${widget.bookMark}",
+                          maxLines: 1,
+                          overflow: TextOverflow.ellipsis, // 溢出的文本用省略号代替
+                          style: TextStyle(
+                            fontFamily: "KTGB2312",
+                            fontSize: 11.0, // 字号
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                  // 最新章节
+                  Row(
+                    children: <Widget>[
+                      Icon(
+                        Icons.move_to_inbox, // 最新章节图标
+                        size: 18.0,
+                      ),
+                      Expanded(
+                        child: Text(
+                          "${widget.bookLastChapter}",
+                          maxLines: 1,
+                          overflow: TextOverflow.ellipsis, // 溢出的文本用省略号代替
+                          style: TextStyle(
+                            fontFamily: "KTGB2312",
+                            fontSize: 11.0, // 字号
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
               ),
-            ],
+            ),
           ),
-        ),
+        ],
       ),
     );
   }
